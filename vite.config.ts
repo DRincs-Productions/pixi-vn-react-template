2dc5c65eb5952061da1bf62da5f948b18d26eabd
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-11-20 10:33:45 +0100

    Initialize Tauri application with basic configuration and structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import checker from 'vite-plugin-checker'
-import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -49,6 +51,28 @@ export default defineConfig({
           'lib/react-markdown': ['react-markdown', 'rehype-raw', 'remark-gfm'],
         },
       },
+    },
+  },
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
     },
   },
 })
