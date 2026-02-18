30deed1b164b288e7b943fac155d149fdf8c6f6e
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-02-18 11:59:22 +0100

    Update @drincs/pixi-vn dependency to version 1.5.11 and adjust animation label properties

--- src/labels/animations-labels.ts
+++ src/labels/animations-labels.ts
@@ -7,7 +7,7 @@ export const animation01 = newLabel("animation_01", [
             {
                 scaleX: 1,
             },
-            { autoplay: false, forceCompleteBeforeNext: true }
+            { autoplay: false, completeOnContinue: true },
         );
 
         await moveIn(
@@ -16,7 +16,7 @@ export const animation01 = newLabel("animation_01", [
                 value: ["fm02-body", "fm02-eyes-joy", "fm02-mouth-smile01"],
                 options: { xAlign: 0.8, yAlign: 1, scale: { y: 1, x: -1 }, anchor: 0.5 },
             },
-            { direction: "right", ease: "easeInOut", tickerIdToResume: tickerId }
+            { direction: "right", ease: "easeInOut", tickerIdToResume: tickerId },
         );
     },
 ]);
