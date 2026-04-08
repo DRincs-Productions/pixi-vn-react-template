2a6b21a8c21838346d9507427ee88ff8990334ba
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-08 21:45:36 +0200

    feat: integrate Game navigation with router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Game } from "@drincs/pixi-vn";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "@/routeTree.gen";
@@ -9,6 +10,7 @@ const router = createRouter({
     scrollRestoration: true,
     context: { queryClient },
 });
+Game.onNavigate((to) => router.navigate({ to }));
 
 declare module "@tanstack/react-router" {
     interface Register {
