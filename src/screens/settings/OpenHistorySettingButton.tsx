abeea011bf7e76124fa87221029527650945f897
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-09 12:16:09 +0200

    feat: simplify OpenHistorySettingButton by removing navigate logic

--- src/screens/settings/OpenHistorySettingButton.tsx
+++ src/screens/settings/OpenHistorySettingButton.tsx
@@ -1,13 +1,12 @@
 import HistoryIcon from "@mui/icons-material/History";
 import { Typography } from "@mui/joy";
-import { useLocation, useNavigate } from "@tanstack/react-router";
+import { useLocation } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 import SettingButton from "../../components/SettingButton";
 import { HistoryScreenStore } from "../../stores/useHistoryScreenStore";
 
 export default function OpenHistorySettingButton() {
     const { t } = useTranslation(["ui"]);
-    const navigate = useNavigate();
 
     const location = useLocation();
     if (location.pathname === "/") {
@@ -15,18 +14,7 @@ export default function OpenHistorySettingButton() {
     }
 
     return (
-        <SettingButton
-            onClick={() => {
-                HistoryScreenStore.toggleOpen();
-                navigate({
-                    search: ((prev: { settings?: true }): { settings?: true } => {
-                        const { settings: _, ...rest } = prev;
-                        return rest;
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    }) as any,
-                });
-            }}
-        >
+        <SettingButton onClick={HistoryScreenStore.toggleOpen}>
             <HistoryIcon />
             <Typography level="title-md">{t("history")}</Typography>
             <Typography
