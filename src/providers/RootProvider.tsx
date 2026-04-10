fb488fc086ff554b2b3a58af2ad72933245a6f8b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-10 17:27:53 +0200

    feat: set position for Toaster component in RootProvider

--- src/providers/RootProvider.tsx
+++ src/providers/RootProvider.tsx
@@ -12,7 +12,7 @@ export default function RootProvider({ children }: { children: React.ReactNode }
                 }}
             >
                 <AlertDialogProvider>{children}</AlertDialogProvider>
-                <Toaster />
+                <Toaster position="top-center" />
             </HotkeysProvider>
         </MyThemeProvider>
     );
