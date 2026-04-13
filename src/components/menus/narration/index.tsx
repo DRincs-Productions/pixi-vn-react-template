48b6485528d2bf5e04bd45d62a8ae878f4e5df50
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-13 22:41:38 +0200

    refactor: integrate useQueryDialogue for conditional rendering in NarrationScreen

--- src/components/menus/narration/index.tsx
+++ src/components/menus/narration/index.tsx
@@ -1,8 +1,11 @@
 import { ChoiceMenu } from "@/components/menus/choice-menus";
 import { NarrationCards } from "@/components/menus/narration/narration-cards";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import { useQueryDialogue } from "@/hooks/useQueryInterface";
 
 export default function NarrationScreen() {
+    const { data: { animatedText, text } = {} } = useQueryDialogue();
+
     return (
         <div className="absolute flex h-full w-full flex-col">
             <div className="flex min-h-0 flex-1 flex-col">
@@ -10,10 +13,12 @@ export default function NarrationScreen() {
                     <ResizablePanel>
                         <ChoiceMenu />
                     </ResizablePanel>
-                    <ResizableHandle />
-                    <ResizablePanel defaultSize={"30%"}>
-                        <NarrationCards />
-                    </ResizablePanel>
+                    {(animatedText || text) && <ResizableHandle className="mx-3" />}
+                    {(animatedText || text) && (
+                        <ResizablePanel defaultSize={"30%"}>
+                            <NarrationCards />
+                        </ResizablePanel>
+                    )}
                 </ResizablePanelGroup>
             </div>
             <div className="h-[0.9rem] shrink-0 sm:h-[1rem] md:h-[1.1rem] lg:h-[1.3rem] xl:h-[1.4rem]" />
