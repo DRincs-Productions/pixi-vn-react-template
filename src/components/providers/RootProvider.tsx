362358664727dfee545f8c761c401d7ab6b1d814
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-19 11:09:40 +0200

    remove mui

--- src/components/providers/RootProvider.tsx
+++ src/components/providers/RootProvider.tsx
@@ -1,25 +1,22 @@
-import { HotkeysProvider } from "@tanstack/react-hotkeys";
 import { AlertDialogProvider } from "@/components/providers/AlertDialogProvider";
-import MyThemeProvider from "@/components/providers/MyThemeProvider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { HotkeysProvider } from "@tanstack/react-hotkeys";
 
 export default function RootProvider({ children }: { children: React.ReactNode }) {
     return (
-        <MyThemeProvider>
-            <ThemeProvider>
-                <HotkeysProvider
-                    defaultOptions={{
-                        hotkey: { preventDefault: true },
-                    }}
-                >
-                    <AlertDialogProvider>
-                        <TooltipProvider>{children}</TooltipProvider>
-                    </AlertDialogProvider>
-                    <Toaster position="top-center" />
-                </HotkeysProvider>
-            </ThemeProvider>
-        </MyThemeProvider>
+        <ThemeProvider>
+            <HotkeysProvider
+                defaultOptions={{
+                    hotkey: { preventDefault: true },
+                }}
+            >
+                <AlertDialogProvider>
+                    <TooltipProvider>{children}</TooltipProvider>
+                </AlertDialogProvider>
+                <Toaster position="top-center" />
+            </HotkeysProvider>
+        </ThemeProvider>
     );
 }
