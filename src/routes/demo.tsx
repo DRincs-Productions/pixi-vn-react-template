09f13774ea74eae014e3ce9642f3bc178bd4e02b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-13 12:14:58 +0200

    improve

--- src/routes/demo.tsx
+++ src/routes/demo.tsx
@@ -1,7 +1,7 @@
 import { startLabel } from "@/content/labels/start-label";
 import { useGameProps } from "@/lib/hooks/props-hooks";
 import { Game } from "@drincs/pixi-vn";
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { useEffect } from "react";
 
 export const Route = createFileRoute("/demo")({
@@ -10,15 +10,16 @@ export const Route = createFileRoute("/demo")({
 
 function Demo() {
     const props = useGameProps();
-    const nevigate = useNavigate();
 
     useEffect(() => {
         setTimeout(async () => {
-            nevigate({ to: "/game/narration" }).then(() => {
-                Game.start(startLabel, props);
+            props.navigate({ to: "/game/narration" }).then(() => {
+                Game.start(startLabel, props).then(() => {
+                    props.invalidateInterfaceData();
+                });
             });
-        }, 1000);
-    }, [props, nevigate]);
+        }, 100);
+    }, [props]);
 
     return null;
 }
