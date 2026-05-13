102e1e4aa2c32f2bb2da18f9a13bff0d0483059b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-13 11:33:50 +0200

    fix

--- src/routes/demo.tsx
+++ src/routes/demo.tsx
@@ -1,7 +1,7 @@
 import { startLabel } from "@/content/labels/start-label";
 import { useGameProps } from "@/lib/hooks/props-hooks";
 import { Game } from "@drincs/pixi-vn";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useEffect } from "react";
 
 export const Route = createFileRoute("/demo")({
@@ -10,12 +10,13 @@ export const Route = createFileRoute("/demo")({
 
 function Demo() {
     const props = useGameProps();
+    const nevigate = useNavigate();
 
     useEffect(() => {
-        props.navigate({ to: "/game/narration" }).then(() => {
+        nevigate({ to: "/game/narration" }).then(() => {
             Game.start(startLabel, props);
         });
-    }, [props]);
+    }, [props, nevigate]);
 
     return null;
 }
