10c6c8dbb903802ce8bce349ef42d3a0f77052da
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-14 15:57:08 +0200

    fix: update Game.start parameter and improve ink initialization effect dependency

--- src/hooks/useInkInitialization.tsx
+++ src/hooks/useInkInitialization.tsx
@@ -1,12 +1,12 @@
+import { initializeInk } from "@/lib/utils/ink-utility";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { initializeInk } from "../utils/ink-utility";
 
 export default function useInkInitialization() {
     const { t } = useTranslation(["narration"]);
     useEffect(() => {
         initializeInk({ t });
-    }, []);
+    }, [t]);
 
     return null;
 }
