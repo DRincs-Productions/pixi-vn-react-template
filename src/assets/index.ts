44a8beab456eba5514fe8271f0d77604fe3a76e7
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-31 10:14:04 +0200

    feat: add asset manifest import for asset management

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -9,7 +9,7 @@ import type { AssetsManifest } from "@drincs/pixi-vn";
  * Manifest for the assets used in the game.
  * You can read more about the manifest here: https://pixijs.com/8.x/guides/components/assets#loading-multiple-assets
  */
-const manifest: AssetsManifest = {
+export const manifest: AssetsManifest = {
     bundles: [
         ...generatedManifestJson.bundles,
         {
@@ -272,5 +272,3 @@ const manifest: AssetsManifest = {
         },
     ],
 };
-
-export default manifest;
